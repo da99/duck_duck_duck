ec9edae58a9a3980a1e728ac7774cfd1d6b82d8d
da99 <i-hate-spam-1234567@mailinator.com>
2013-04-30 16:10:33 -0500

    Added: test helpers.

--- app.js
+++ app.js
@@ -2,12 +2,31 @@
 var _  = require('underscore')
 , path = require('path')
 , fs = require('fs')
+, River = require('da_river').River
+, Topogo = require('topogo').Topogo
 ;
 
+var schema_table    = process.env.MIGRATE_TABLE || '_schema';
 var MIGRATE_PATTERN = /^\d+\-/;
-var name = path.basename(process.cwd());
-var migrates = _.select(fs.readdirSync("migrates"), function (file, i) {
+var name            = path.basename(process.cwd());
+var migrates        = _.select(fs.readdirSync("migrates"), function (file, i) {
   return file.match(MIGRATE_PATTERN);
 }).sort();
 
-console.log(name, migrates);
+
+River.new(null)
+.job(function (j) {
+  Topogo.run('CREATE TABLE IF NOT EXISTS ' + schema_table + ' (' +
+             ' name varchar(255) NOT NULL UNIQUE ,   ' +
+             ' version smallint NOT NULL DEFAULT 0 ' +
+             ')', [], j);
+})
+// .job(function (j, last) {
+  // console.log(name, migrates);
+  // console.log(last, last[0]);
+  // j.finish();
+// })
+.run(function () {
+  Topogo.close();
+});
+
---
+++ tests/duck_duck_duck.js
@@ -0,0 +1,31 @@
+
+var _    = require('underscore')
+, Topogo = require('topogo').Topogo
+, River = require('da_river').River
+, assert = require('assert')
+;
+
+
+describe( 'schema table', function () {
+  it( 'gets created', function (done) {
+    River.new(null)
+    .job(function (j) {
+      Topogo.run('SELECT * FROM _test_schema', [], j);
+    })
+    .job(function (j, last) {
+      assert.equal(last.length > 0, true);
+    })
+    .run();
+  });
+
+  it( 'creates rows with: name, version', function (done) {
+    River.new(null)
+    .job(function (j) {
+      Topogo.run('SELECT * FROM _test_schema', [], j);
+    })
+    .job(function (j, last) {
+      assert.deepEqual(_.keys(last[0]), ['name', 'version']);
+    })
+    .run();
+  });
+}); // === end desc
