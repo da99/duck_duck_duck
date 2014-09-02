88016c15de0bf9eaa5ed7fb60951cb047b41c857
da99 <i-hate-spam-1234567@mailinator.com>
2014-09-01 22:57:19 -0500

    Added: use of Exit_0 to ensure commands exit w/ 0

--- duck_duck_duck.gemspec
+++ duck_duck_duck.gemspec
@@ -25,9 +25,10 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "pry"           , "~> 0.9"
   spec.add_development_dependency "rake"          , "~> 10.3"
   spec.add_development_dependency "bundler"       , "~> 1.5"
-  spec.add_development_dependency "bacon"         , "~> 1.0"
+  spec.add_development_dependency "bacon"         , "~> 1.2.0"
   spec.add_development_dependency "Bacon_Colored" , "~> 0.1"
 
   spec.add_development_dependency "sequel"        , "~> 4.13"
   spec.add_development_dependency "pg"            , "~> 0.16"
+  spec.add_development_dependency "Exit_0"        , ">= 1.4.1"
 end
--- specs/duck_duck_duck.rb
+++ specs/duck_duck_duck.rb
@@ -1,5 +1,7 @@
 
 require 'sequel'
+require 'Exit_0'
+
 schema = ENV['SCHEMA_TABLE'] = '_test_schema'
 DB = Sequel.connect ENV['DATABASE_URL']
 MODELS = Dir.glob('*/migrates').map { |dir|  File.basename File.dirname(dir) }
@@ -37,21 +39,21 @@ describe "create" do
 
   before {
     tmp_dir = '/tmp/ddd_ver'
-    `rm -fr #{tmp_dir}`
-    `mkdir -p #{tmp_dir}`
+    Exit_0("rm -fr #{tmp_dir}")
+    Exit_0("mkdir -p #{tmp_dir}")
     @dir = tmp_dir
   }
 
   it "names the file in successive file versions: 0000-....sql" do
     Dir.chdir(@dir) {
-      `duck_duck_duck create MOD table_1`
-      `duck_duck_duck create MOD table_2`
+      Exit_0("duck_duck_duck create MOD table_1")
+      Exit_0("duck_duck_duck create MOD table_2")
 
-      `touch MOD/migrates/0022-skip_zero.sql`
-      `duck_duck_duck create MOD table_3`
+      Exit_0("touch MOD/migrates/0022-skip_zero.sql")
+      Exit_0("duck_duck_duck create MOD table_3")
 
-      `touch MOD/migrates/0091-skip_zero.sql`
-      `duck_duck_duck create MOD table_100`
+      Exit_0("touch MOD/migrates/0091-skip_zero.sql")
+      Exit_0("duck_duck_duck create MOD table_100")
 
       File.should.exists('MOD/migrates/0010-table_1.sql')
       File.should.exists('MOD/migrates/0020-table_2.sql')
@@ -67,16 +69,16 @@ describe 'up model' do
   before { reset }
 
   it( 'updates version to latest migration' ) do
-    `duck_duck_duck up 0010_model`
+    Exit_0("duck_duck_duck up 0010_model")
     get('SELECT * FROM _test_schema').
       first[:version].should == versions('0010_model').last
   end
 
   it 'does not run migrations from previous versions' do
-    `duck_duck_duck migrate_schema`
+    Exit_0("duck_duck_duck migrate_schema")
     DB << File.read("0010_model/migrates/0010-table.sql").split('-- DOWN').first
     DB << "INSERT INTO #{schema.inspect} VALUES ('0010_model', '20');"
-    `duck_duck_duck up 0010_model`
+    Exit_0("duck_duck_duck up 0010_model")
     get('SELECT * FROM "0010_model"', :title).
       should == ['record 30', 'record 40', 'record 50']
   end
@@ -88,25 +90,25 @@ describe 'down model' do
   before { reset }
 
   it 'leaves version to 0' do
-    `duck_duck_duck up 0010_model`
-    `duck_duck_duck down 0010_model`
+    Exit_0("duck_duck_duck up 0010_model")
+    Exit_0("duck_duck_duck down 0010_model")
     get(%^SELECT * FROM #{schema.inspect} WHERE name = '0010_model'^, :version).last.
       should == 0
   end
 
   it 'runs migrates in reverse order' do
-    `duck_duck_duck up 0020_model`
-    `duck_duck_duck down 0020_model`
+    Exit_0("duck_duck_duck up 0020_model")
+    Exit_0("duck_duck_duck down 0020_model")
     get('SELECT * FROM "0020_model"', :title).
       should == ['record 20', 'record 30', 'DROP record 30', 'DROP record 20', 'DROP 0020_model']
   end
 
   it 'does not run down migrates from later versions' do
-    `duck_duck_duck migrate_schema`
+    Exit_0("duck_duck_duck migrate_schema")
     DB << File.read("0020_model/migrates/0010-table.sql").split('-- DOWN').first
     DB << "INSERT INTO #{schema.inspect} VALUES ('0020_model', '20');"
     DB << "UPDATE #{schema} SET version = '20' WHERE name = '0020_model';"
-    `duck_duck_duck down 0020_model`
+    Exit_0("duck_duck_duck down 0020_model")
     get('SELECT * FROM "0020_model"', :title).
       should == ['DROP record 20', 'DROP 0020_model']
   end
@@ -118,7 +120,7 @@ describe "up" do
   before { reset }
 
   it "migrates all models" do
-    `duck_duck_duck up`
+    Exit_0("duck_duck_duck up")
     get("SELECT * FROM #{schema.inspect} ORDER BY name").
       should == [
         {:name=>'0010_model',:version=>50},
@@ -134,8 +136,8 @@ describe 'down' do
   before { reset }
 
   it "migrates down all models" do
-    `duck_duck_duck up`
-    `duck_duck_duck down`
+    Exit_0("duck_duck_duck up")
+    Exit_0("duck_duck_duck down")
     get("SELECT * FROM #{schema.inspect} ORDER BY name").
       should == [
         {:name=>'0010_model',:version=>0},
